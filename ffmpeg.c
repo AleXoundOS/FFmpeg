75ae10bda787c0416c203d9816e28cf5ca537416
Alexander Tomokhov <alexoundos@gmail.com>
2016-04-17 01:31:15 +0300

    remove "q=" from info line for codec_type == AVMEDIA_TYPE_VIDEO

--- ffmpeg.c
+++ ffmpeg.c
@@ -1555,8 +1555,8 @@ static void print_report(int is_last_report, int64_t timer_start, int64_t cur_ti
 
             frame_number = ost->frame_number;
             fps = t > 1 ? frame_number / t : 0;
-            snprintf(buf + strlen(buf), sizeof(buf) - strlen(buf), "frame=%5d fps=%3.*f q=%3.1f ",
-                     frame_number, fps < 9.95, fps, q);
+            snprintf(buf + strlen(buf), sizeof(buf) - strlen(buf), "frame=%5d fps=%3.*f ",
+                     frame_number, fps < 9.95, fps);
             av_bprintf(&buf_script, "frame=%d\n", frame_number);
             av_bprintf(&buf_script, "fps=%.1f\n", fps);
             av_bprintf(&buf_script, "stream_%d_%d_q=%.1f\n",
